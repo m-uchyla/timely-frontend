a435af63c656edc368e0d4e73cd4cf147be9f5f5
m-uchyla <czarnytroll@gmail.com>
2025-08-07 13:13:18 +0200

    services

--- app/(default)/services/service-card.tsx
+++ app/(default)/services/service-card.tsx
@@ -30,22 +30,6 @@ export default function ServiceCard({ service }: { service: Service }) {
   return (
     <div className="col-span-full sm:col-span-6 xl:col-span-4 bg-white dark:bg-gray-800 shadow-sm rounded-xl">
       <div className="flex flex-col h-full p-5">
-        <header>
-          <div className="flex items-center justify-between">
-            {categoryIcon(service.category)}
-            <div className="flex shrink-0 -space-x-3 -ml-px">
-              {
-                service.members.map(member => {
-                  return (
-                    <Link key={member.name} className="block" href={member.link}>
-                      <Image className="rounded-full border-2 border-white dark:border-gray-800 box-content" src={member.image} width={28} height={28} alt={member.name} />
-                    </Link>
-                  )
-                })
-              }
-            </div>
-          </div>
-        </header>
         <div className="grow mt-2">
           <Link className="inline-flex text-gray-800 dark:text-gray-100 hover:text-gray-900 dark:hover:text-white mb-1" href={service.link}>
             <h2 className="text-xl leading-snug font-semibold">{service.title}</h2>
@@ -53,7 +37,7 @@ export default function ServiceCard({ service }: { service: Service }) {
           <div className="text-sm">{service.content}</div>
         </div>
         <footer className="mt-5">
-          <div className="text-sm font-medium text-gray-500 mb-2">{service.dates.from} <span className="text-gray-400 dark:text-gray-600">-&gt;</span> {service.dates.to}</div>
+          <div className="text-sm font-medium text-gray-500 mb-2">{service.dates.from} <span className="text-gray-400 dark:text-gray-600">-&gt;</span> {service.dates.to} zł</div>
           <div className="flex justify-between items-center">
             <div>
               <div className={`text-xs inline-flex font-medium rounded-full text-center px-2.5 py-1 ${typeColor(service.type)}`}>{service.type}</div>
